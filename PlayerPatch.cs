1e84f668f33ba22067c1a0df01280da0860e1085
Oinky <contact.knilax@gmail.com>
2024-05-09 01:01:16 -0500

    Added invincibility

---
+++ PlayerPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace SpeedrunTools
+{
+    internal class PlayerPatch
+    {
+        [HarmonyPatch(typeof(Player), "health", MethodType.Setter)]
+        public static bool SetHealth(Player __instance, float value)
+        {
+            if (value > __instance.health)
+                return true;
+            else
+                return false;
+
+        } // end SetHealth
+
+    } // end class PlayerPatch
+
+} // end namespace
