93c449abc22787a0182f0374689ee3a93a7ee823
Devin <superdevin00@gmail.com>
2024-05-11 00:22:47 -0500

    CAL Trainer
    Added a call trainer with 3 settings
    - Off
    - Visual
    - Automatic

--- SpeedrunTools.cs
+++ SpeedrunTools.cs
@@ -10,20 +10,29 @@ namespace SpeedrunTools
         /// <summary>
         /// Features with an associated <see cref="KeyCodes"/> value for input.
         /// </summary>
-        internal enum Tools { GodMode }
+        internal enum Tools { GodMode, CALTrainer }
 
         /// <summary>
         /// Holds <see cref="KeyCode"/> for features that have an input key.
         /// </summary>
         internal static Dictionary<Tools, KeyCode> KeyCodes => new()
         {
-            { Tools.GodMode, KeyCode.F1 }
+            { Tools.GodMode, KeyCode.F1 },
+            { Tools.CALTrainer, KeyCode.F2}
+
         };
+
+
 
         /// <summary>
         /// Whether god mode (invincibility) is currently enabled.
         /// </summary>
         internal static bool GodMode { get; set; } = true;
+
+        /// <summary>
+        /// What Mode the CAL Trainer is in
+        /// </summary>
+        internal static int CalTrainer { get; set; } = 0;
 
     } // end class SpeedrunTools
 
